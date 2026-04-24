4f70ee69d3862800bb0f7ec84ad392f71d3d7e91
Kvasz <kvaszbarnabas@gmail.com>
2026-04-24 09:37:12 +0200

    Initialize

---
+++ common/defines/undr_defines.lua
@@ -0,0 +1,34 @@
+-- Define overrides when creating Custom Nation
+
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NNationDesigner.POINTS_AVAILABLE = 20000
+NDefines.NNationDesigner.RULER_SKILL_COST_MODIFIER = 0
+NDefines.NNationDesigner.ELECTION_SKILL_COST_MODIFIER = 0
+NDefines.NNationDesigner.CONSORT_COST_MULTIPLIER = 0
+NDefines.NNationDesigner.BASE_TAX_COST_MODIFIER = 0
+NDefines.NNationDesigner.PRODUCTION_COST_MODIFIER = 0
+NDefines.NNationDesigner.MANPOWER_COST_MODIFIER = 0
+NDefines.NNationDesigner.GOLD_PROVINCE_COST_MULTIPLIER = 0
+NDefines.NNationDesigner.TECH_GROUP_COST_MODIFIER = 0
+NDefines.NNationDesigner.MEMBER_OF_HRE_COST = 0
+NDefines.NNationDesigner.MEMBER_OF_HRE_PROVINCE_COST = 0
+NDefines.NNationDesigner.TECH_GROUP_NO_NEIGHBOUR_COST = 0
+NDefines.NNationDesigner.IDEAS_PERCENTAGE_LIMIT = 0
+NDefines.NNationDesigner.IDEAS_LIMIT_PENALTY = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_1 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_2 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_3 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_4 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_5 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_6 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_7 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_8 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_9 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_10 = 0
+NDefines.NNationDesigner.MAX_DISTANCE_TO_OWNER_AREA = 12000
+NDefines.NNationDesigner.GOVERNMENT_RANK_COST_1 = 0
+NDefines.NNationDesigner.GOVERNMENT_RANK_COST_2 = 0
+NDefines.NNationDesigner.GOVERNMENT_RANK_COST_3 = 0
